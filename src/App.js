541c3ef391c5aac115cebcf0782e81f93424d1e0
CN22 <codenation@CN22s-MacBook-Pro.local>
2020-02-17 10:34:14 +0000

    finish commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ResultComponent from "./components/ResultComponent";
+import KeyPadComponent from "./components/KeyPadComponents";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    result: ""
+  };
+
+  onClick = button => {
+    if (button === "=") {
+      this.calculate();
+    } else if (button === "C") {
+      this.reset();
+    } else if (button === "CE") {
+      this.backspace();
+    } else {
+      this.setState({
+        result: this.state.result + button
+      });
+    }
+  };
+
+  calculate = () => {
+    let checkResult = "";
+    if (this.state.result.includes("--")) {
+      checkResult = this.state.result.replace("--", "+");
+    } else {
+      checkResult = this.state.result;
+    }
+    // Try lets you test a block of code for errors
+    try {
+      this.setState({
+        result: (eval(checkResult) || "") + ""
+      });
+      // catch lest you handle the error
+    } catch (e) {
+      this.setState({
+        result: "error"
+      });
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    });
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="calculator-body">
+          <h1> Simple Calculator </h1>
+          <ResultComponent result={this.state.result} />
+
+          <KeyPadComponent onClick={this.onClick} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
