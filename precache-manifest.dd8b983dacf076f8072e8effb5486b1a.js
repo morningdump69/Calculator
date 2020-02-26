cc259eaa74657eeeeb6079f6bf8b3fc72c58beb8
rossburrows <rossburrows96@gmail.com>
2020-02-26 17:16:57 +0000

    Updates

---
+++ precache-manifest.dd8b983dacf076f8072e8effb5486b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba039e1b14d992901b1742737d3aa5c",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "13a6b45c0dfc730626da",
+    "url": "/Calculator/static/css/main.da1bbf59.chunk.css"
+  },
+  {
+    "revision": "91437916b2e9c943958e",
+    "url": "/Calculator/static/js/2.2c2680ee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Calculator/static/js/2.2c2680ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a6b45c0dfc730626da",
+    "url": "/Calculator/static/js/main.2e7dc065.chunk.js"
+  },
+  {
+    "revision": "a0843c05571dcd8658af",
+    "url": "/Calculator/static/js/runtime-main.939bd12a.js"
+  }
+]);
